08fc5c4dccb9483713cac0665e2fec9db0c7cb94
ChegeDaniella <chegedaniella@gmail.com>
2020-03-05 01:01:53 +0300

    interface completed

---
+++ js/membership.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".head-gold").click(function(){
+        $(".form-gold").slideToggle();
+    });
+    $(".head-silver").click(function(){
+        $(".form-silver").slideToggle();
+    });
+    $(".head-platinum").click(function(){
+        $(".form-platinum").slideToggle();
+    });
+
+})
