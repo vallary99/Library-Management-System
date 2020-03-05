0cba7edc84653137b857789515c7b0e1f3fc0d59
ChegeDaniella <chegedaniella@gmail.com>
2020-03-05 16:55:07 +0300

    modals

--- js/scripts.js
+++ js/scripts.js
@@ -10,18 +10,23 @@ $(document).ready(function(){
     });
 
 });
-$(".submit").click(function(){
-    var name=$("#fname").val();
-    var email=$("#email").val();
-    var key='f2a28d47f48fba910a882d260d1675be-us19'
-    if(name === ""){
-        alert("please fill in your name")
-    }else if(email === ""){
-        alert("Please enter your email")
-    }else {
-        alert("Thank you"+" "+name+" "+"."+" "+"An email has been sent to"+" "+email)
-    }
-})
+function validate(){
+    var name=document.getElementById("fname").Value;
+    var email=document.getElementById("email").Value;
+     confirm(name +" "+email)
+}
+// $(".submit").click(function(){
+//     var name=$("#fname").val();
+//     var email=$("#email").val();
+//     var key='f2a28d47f48fba910a882d260d1675be-us19'
+//     if(name === ""){
+//         alert("please fill in your name")
+//     }else if(email === ""){
+//         alert("Please enter your email")
+//     }else {
+//         alert("Thank you"+" "+name+" "+"."+" "+"An email has been sent to"+" "+email)
+//     }
+// })
 $(".submit2").click(function(){
     var name2=$("#fname2").val();
     var email2=$("#email2").val();
@@ -46,6 +51,24 @@ $(".submit3").click(function(){
         alert("Thank you"+" "+name3+" "+"."+" "+"An email has been sent to"+" "+email3)
     }
 })
+
+// Gold modal
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
 var name;
 var pasw;
 var pasw2;
@@ -69,22 +92,38 @@ function clear(){
     clear()
 });
 
+// silver modal
+  var modal2 = document.getElementById("myModal2");
+var btn2 = document.getElementById("myBtn2");
+var span2 = document.getElementsByClassName("close2")[0];
 
-
-
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-    modal.style.display = "block";
+btn2.onclick = function() {
+    modal2.style.display = "block";
   }
-  span.onclick = function() {
-    modal.style.display = "none";
+  span2.onclick = function() {
+    modal2.style.display = "none";
   }
   window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+    if (event.target == modal2) {
+      modal2.style.display = "none";
     }
   }
+  
+// platinum modal
+  var modal3 = document.getElementById("myModal3");
+  var btn3 = document.getElementById("myBtn3");
+  var span3 = document.getElementsByClassName("close3")[0];
+  
+  btn3.onclick = function() {
+      modal3.style.display = "block";
+    }
+    span3.onclick = function() {
+      modal3.style.display = "none";
+    }
+    window.onclick = function(event) {
+      if (event.target == modal3) {
+        modal3.style.display = "none";
+      }
+    } 
+
 
